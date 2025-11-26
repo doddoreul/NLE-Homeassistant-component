ff8f6d89c70cc24888ad1c6df1851d92f0d74cd0
doddoreul <doddoreul@users.noreply.github.com>
2025-11-26 15:56:12 +0200

    Adding helpers

--- custom_components/nle_thermostat/__init__.py
+++ custom_components/nle_thermostat/__init__.py
@@ -2,6 +2,7 @@
 import logging
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.typing import ConfigType
+from homeassistant.helpers import discovery
 
 from .const import DOMAIN
 from .config import get_config

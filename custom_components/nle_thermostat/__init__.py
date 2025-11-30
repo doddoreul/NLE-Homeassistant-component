224399b629cb574be4238e4b562f79c6e522dcb2
doddoreul <doddoreul@users.noreply.github.com>
2025-11-30 16:37:21 +0200

    Trying to force climate to exist

--- custom_components/nle_thermostat/__init__.py
+++ custom_components/nle_thermostat/__init__.py
@@ -11,9 +11,15 @@ _LOGGER = logging.getLogger(__name__)
 
 async def async_setup(hass: HomeAssistant, config: ConfigType):
     """Chargement via configuration.yaml uniquement."""
+    # Récupération de la config YAML
     hass.data[DOMAIN] = get_config(hass, config)
 
+    # Forward setup pour sensor
     await discovery.async_load_platform(hass, "sensor", DOMAIN, {}, config)
-    _LOGGER.info("NLE Thermostat loaded")
+    _LOGGER.info("NLE Thermostat sensors loaded")
+
+    # Forward setup pour climate
+    await discovery.async_load_platform(hass, "climate", DOMAIN, {}, config)
+    _LOGGER.info("NLE Thermostat climate loaded")
 
     return True
